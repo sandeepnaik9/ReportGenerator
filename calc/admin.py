b03a7137c5988124ecd28845e25935f8ae5270c4
sandeepnaik9 <sandeepnaik9900@gmail.com>
2021-06-13 13:07:11 +0530

    master

--- calc/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from calc.models import Profile,LoggedInUser
-# Register your models here.
-
-
-admin.site.register(Profile)
-admin.site.register(LoggedInUser)
--- calc/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class CalcConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'calc'
-
-    def ready(self):
-        import calc.signals
